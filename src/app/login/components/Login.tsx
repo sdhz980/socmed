33d84dd45cd0e461df1ffa52bdf8ed5fe81e5ae6
SDHZed <83025970+sdhz980@users.noreply.github.com>
2024-03-21 05:59:50 +0700

    Update Login.tsx

--- src/app/login/components/Login.tsx
+++ src/app/login/components/Login.tsx
@@ -12,8 +12,8 @@ const Login = () => {
     const router = useRouter();
 
     const validation = Yup.object().shape({
-        username: Yup.string().min(6).required(),
-        password: Yup.string().required().min(6).required()
+        username: Yup.string().required("please input yout username"),
+        password: Yup.string().required("please input yout password")
       })
     
       const formik = useFormik({
@@ -58,10 +58,12 @@ const Login = () => {
                         <div>
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username</label>
                             <input type="text" name="username" value={formik.values.username} onChange={formik.handleChange} id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="your username" />
+                            <p className="text-sm font-light text-gray-500 dark:text-red-400"> {formik.errors.username} </p>
                         </div>
                         <div>
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                             <input type="password" name="password" onChange={formik.handleChange} value={formik.values.password} id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                            <p className="text-sm font-light text-gray-500 dark:text-red-400"> {formik.errors.password} </p>
                         </div>
                         <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Login</button>
                         <p className="text-sm font-light text-gray-500 dark:text-gray-400">
